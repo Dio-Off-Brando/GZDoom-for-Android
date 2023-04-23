ddd0a1531a20b68bef2a6dec1af63d61195aeac9
GComplex <somedudz@protonmail.com>
2023-04-23 19:28:16 +0200

    added latest openal-soft

--- doom/src/main/jni/Android.mk
+++ doom/src/main/jni/Android.mk
@@ -10,7 +10,7 @@ include $(TOP_DIR)/gzdoom_android/Android.mk
 
 include $(TOP_DIR)/FMOD_studio/Android.mk
 include $(TOP_DIR)/fluidsynth/Android.mk
-include $(TOP_DIR)/openal-soft-android/Android.mk
+# include $(TOP_DIR)/openal-soft-android/Android.mk
 
 include $(TOP_DIR)/MobileTouchControls/Android.mk
 
